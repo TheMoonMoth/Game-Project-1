7803ef3acd886730ae0e4dbbca6f3b2becc1aa3f
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-03-05 15:07:51 -0700

    setup

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("Player")) {
+			//chance to add a sound or score for the player
+			Destroy (gameObject);
+		}
+	}
+}
